b45da50108e36990603e9cb0e91db704e6ec0cc8
Abrehg <100381257+Abrehg@users.noreply.github.com>
2022-05-23 15:23:35 -0400

    Update CreateService.py

--- CreateService.py
+++ CreateService.py
@@ -1,10 +1,15 @@
 from Google import create_service
 
 def gmailServiceCreate():
-    CLIENT_FILE = """Enter client ID here"""
+    CLIENT_FILE = 'client.json'
     API_NAME = 'gmail'
     API_VERSION = 'v1'
     SCOPES = ['https://mail.google.com/']
 
     service = create_service(CLIENT_FILE, API_NAME, API_VERSION, SCOPES)
     return service
+
+Inter = "Label_267554376139622606"
+Promo = "Label_4549946469515729599"
+College = "Label_2759360573854140636"
+Inbox = "INBOX"
